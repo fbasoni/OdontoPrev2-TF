4b8d02a991f2527b1f73e0b5bc2df2e0381dcc01
fabi basoni <basoni.fabiany@gmail.com>
2022-11-28 18:57:04 -0300

    inclui db e template do feed de dentista, imprimindo alguns appointments

---
+++ src/lib/data/data.js
@@ -0,0 +1,43 @@
+export default {
+  dentists: [
+    {
+      userId: "",
+      address: "R. Cerro Corá, 43, São Paulo",
+      cro: "PI-4438",
+      name: "Dra. Aline Batista",
+      schedule: [
+        {
+          date: "3 de dezembro de 2022",
+          time: "11:00",
+        },
+      ],
+      specialty: "Clínico",
+    },
+    {
+      address: "R. Cerro Corá, 43, São Paulo",
+      cro: "PI-4438",
+      name: "Dra. Josefina Abreu",
+      schedule: [
+        {
+          date: "5 de dezembro de 2022",
+          time: "13:00",
+        },
+      ],
+      specialty: "Clínico",
+    },
+  ],
+  availability: [
+    {
+      date: "23 de dezembro de 2022",
+      dentistId: "uidabc123",
+      patientId: "uidabc123",
+      status: "",
+      currentUserId: "uidabcde1234",
+    },
+  ],
+  patients: [
+    {
+      userId: "",
+    },
+  ],
+};
--- src/pages/appointment/appointment.js
+++ src/pages/appointment/appointment.js
@@ -1,10 +1,47 @@
+import data from "../../lib/data/data.js";
+
+const dentistsData = data.dentists;
+console.log(dentistsData);
+
 export default () => {
-    const container = document.createElement('div');    
-    const template = `    
-        <p>marcação de consulta</p>
+  const container = document.createElement("div");
+  const template = `    
+        <div class="appointment-container">
+          <span class="dentist-info">
+            <img src="./assets/icons/others/user-female.svg" alt="dentist picture">
+            <p class="dentist-name">${dentistsData[0].name}</p>
+            <p class="dentist-cro">${dentistsData[0].cro}</p>
+          </span>
+          <section class="schedule">
+            <h1>Agenda de consultas</h1>
+            <button class="confirmed-appointments">Confirmadas</button>
+            <button class="pending-appointments">Pendentes</button>
+            <span class="appointment-status">Consultas ??</span>
+            <div class="appointment-info">
+            </div>
+          </section>
+        </div>
       `;
-    container.innerHTML = template; 
+  container.innerHTML = template;
 
+  const appointmentInfo = container.querySelector(".appointment-info");
 
-    return container;
+  const printAppointment = (data) => {
+    const appointmentTemplate = data
+      .map((dentist) => {
+        appointmentInfo.innerHTML = `
+        <div>
+          <p class="patient-name">Paciente: ${dentist.name}</p>
+          <p class="appointment-date">Dia da consulta: ${dentist.schedule[0].date}</p>
+          <p class="appointment-time">Horário da consulta: ${dentist.schedule[0].time}</p>
+        </div>
+        `;
+      })
+      .join("");
+    return appointmentTemplate;
   };
+
+  printAppointment(dentistsData);
+
+  return container;
+};
