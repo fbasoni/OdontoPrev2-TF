e05b83127ea8e34f70705d1ab38835a8e896700b
fabi basoni <basoni.fabiany@gmail.com>
2022-11-29 14:19:35 -0300

    flip da logo funcionando nas páginas de login

--- src/pages/login/patient/login-patient.js
+++ src/pages/login/patient/login-patient.js
@@ -3,14 +3,27 @@ export default () => {
   containerLoginPatient.classList.add('container-login-patient')    
   const templateLoginPatient = `    
   <div class="box">
+    <div class="box">    
     <div class="login-logo">
-      <img class="logo-login" src="./assets/logo/logo-art.svg" alt="logo">
+      <div class="flip-card">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
+            <img class="logo-img" src="./assets/logo/logo-art.svg" alt="logo">
+          </div>
+        <div class="flip-card-back">
+          <h1>OdontoPrev</h1>
+          <p>O nosso desejo é</p>
+          <p>despertar sorrisos</p>
+        </div>
+      </div>
     </div>
+  </div> 
+  
     <form class="login-form">
       <p class="welcome">Bem-vindo de volta!</p>
       <input type="email" placeholder="Digite seu e-mail" id="input-email" class="input-login-patient">
       <input type="password" placeholder="Digite sua senha" id="input-password" class="input-login-patient">
-      <a href="" class="forgot-password" id="id-forgot-password">Esqueci minha senha.</a>
+      <a href="" class="forgot-password" id="id-forgot-password">Esqueci minha senha</a>
       <p class="error-message-text" id="error-message"></p>
 
       <p class="text-patient-login">É beneficiário e não possui uma conta?</p>
