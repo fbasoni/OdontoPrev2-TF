78c3fec0bdd379bfc0cf1d6c0a1e9c968a3773ba
Juliene-Araujo <julienefjs@gmail.com>
2022-11-29 20:39:45 -0300

    CSS/home/carrosel

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -23,13 +23,57 @@ export default () => {
         <span>
       </div>
       <picture class="slider">
-        <img src="assets/logo/foto.jpeg">
+        <img class="selected" src="assets/logo/foto.jpeg">
+        <img src="assets/logo/foto2.jpeg">
+        <img src="assets/logo/foto3.jpeg">
       </picture>
     </main>  
   </div>
     `;
   container.innerHTML = template;
 
+  let time = 5000;
+  let imageIndex = 0;
+  let imageSelected = container.querySelectorAll('.slider img');
+  let maxImage = imageSelected.length;
+
+  const nextImage = () => {
+    imageSelected[imageIndex].classList.remove('selected');
+    console.log('dentro da função')
+    imageIndex++
+    if (imageIndex >= maxImage) {
+      imageIndex = 0;
+    }
+    imageSelected[imageIndex].classList.add('selected');
+    console.log('dentro da função nova')
+  }
+
+  const start = () => {
+    console.log('teste');
+    setInterval(() => { nextImage() }, time);
+  }
+  console.log(start);
+
+  start()
+
+  // let slideIndex = 0;
+  // carousel();
+
+  // function carousel() {
+  //   let i;
+  //   let x = container.getElementsByClassName("mySlides");
+  //   for (i = 0; i < x.length; i++) {
+  //     x[i].style.display = "none";
+  //   }
+  //   slideIndex++;
+  //   if (slideIndex > x.length) { slideIndex = 1 }
+  //   x[slideIndex - 1].style.display = "block";
+  //   setTimeout(carousel, 2000); // Change image every 2 seconds
+  // }
+
+  // setTimeout(() => {
+  //   window.location.hash = '#welcome';
+  // }, 2000);
 
   return container;
 };
