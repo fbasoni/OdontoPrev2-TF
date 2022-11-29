75f683d0b738a1f671dad3adcba46c8fd84f7e43
Tamyres <104659395+Tamyresfmelo@users.noreply.github.com>
2022-11-29 11:19:32 -0300

    Revert "homepage css"

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,26 +2,17 @@
 export default () => {
   const container = document.createElement('div');    
   const template = `    
-  <section class="home">
-  <div class="home-text">
-      <h1 class="text-h1">Especialistas em odontologia</h1>
-      <p>Grupo Odontoprev: líder em planos odontologicos na América Latina.
-          O plano dental mais completo para você, sua família e seus colaboradores, com qualidade e segurança. 
-          Um novo jeito de cuidar da sua saúde bucal.</p>
-
-    
-      <a href='#login-patient' class="home-btn">
-          Beneficiário
-        </a>
-      <a href="#login-dentist" class="home-btn">
-          Dentista
-        </a> 
-
-  </div>
-  <div class="home-img">
-      <img src="/src/assets/img/slider (1).jpg" alt="hamburguer">
-  </div>
-</section>
+    <h1 class="h1"> Especialistas em odontologia </h1>
+    <p class="text"> Grupo Odontoprev: líder em planos odontologicos na América Latina.
+      O plano dental mais completo para você, sua família e seus colaboradores, com qualidade e segurança. 
+      Um novo jeito de cuidar da sua saúde bucal.
+    </p>
+    <a href='#login-patient' class="btn-Beneficiário">
+      <button>Beneficiário</button>
+    </a>
+    <a href="#login-dentist" class="btn-Dentista">
+      <button>Dentista</button>
+    </a> 
     `;
   container.innerHTML = template; 
 
@@ -33,11 +24,3 @@ export default () => {
 
   return container;
 };
-
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu');
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
